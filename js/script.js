985fb3ce3f339cc369a885c1740ab4a71ea60766
Lena Pilyushenko <elena_pilyushenko@mail.ru>
2020-11-24 15:55:31 +0300

    fix(project):удалила ненужноеэ

--- js/script.js
+++
@@ -1,7 +0,0 @@
-let num = 266219;
-let newNum = String(num).split('');
-let mult = newNum.reduce(function(previousValue, item) { 
-    return previousValue * item;
-}, 1);
-console.log('произведение элементов числа  =  ' + mult); 
-console.log(String(mult **= 3).substr(0,2));
