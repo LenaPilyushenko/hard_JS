ca694a59a653ddea09a49ad17b1a7418dc343f0e
Lena Pilyushenko <elena_pilyushenko@mail.ru>
2020-11-28 16:54:38 +0300

    feat(js/lesson07.js):new feat

---
+++ js/lesson07.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const week = ['Понедельние','Вторник','Среда','Четверг','Пятница','Суббота','Воскресенье'];
+const toDay = new Date().getDay() - 1;
+
+for (let i = 0; i < week.length; i++) {
+  if (i === toDay) {
+    if (week[i] === 'сб' || week[i] === 'вс') {
+      document.write(`<p><b><i>${week[i]}</i></b></p>`);
+    } else {
+      document.write(`<p><b>${week[i]}</b></p>`);
+    }
+  } else if (week[i] === 'сб' || week[i] === 'вс') {
+    document.write(`<p><b>${week[i]}</b></p>`);
+  } else {
+    document.write(`<p>${week[i]}</p>`);
+  }
+}
+console.log(week);
+
+// я нашла метод .bold() - но он опять-таки ввыводит
+// в консоль значение в тегаз <b></b>  а не жирным, 
+// поэтому вывела на страницу. не знаю, насколько правильно,
+// в зд вообще написано на экран, а в каком виде не поняла, как сделала так и поняла..
