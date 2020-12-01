7e4398cf78058daa0e3b55ae235d89eb01d776f6
Lena Pilyushenko <elena_pilyushenko@mail.ru>
2020-12-01 21:55:46 +0300

    feat(js/script)

--- js/script.js
+++ js/script.js
@@ -1,24 +1,63 @@
 'use strict';
 
-const week = ['Понедельние','Вторник','Среда','Четверг','Пятница','Суббота','Воскресенье'];
-const toDay = new Date().getDay() - 1;
+//  'Сегодня Вторник, 4 февраля 2020 года, 21 час 5 минут 33 секунды'  
 
-for (let i = 0; i < week.length; i++) {
-  if (i === toDay) {
-    if (week[i] === 'Суббота' || week[i] === 'Воскресенье') {
-      document.write(`<p><b><i>${week[i]}</i></b></p>`);
-    } else {
-      document.write(`<p><b>${week[i]}</b></p>`);
-    }
-  } else if (week[i] === 'Суббота' || week[i] === 'Воскресенье') {
-    document.write(`<p><b>${week[i]}</b></p>`);
-  } else {
-    document.write(`<p>${week[i]}</p>`);
+const nowDate = function () {
+
+const today = new Date(),
+      currentYear = today.getFullYear(),
+      currentMonth = today.toLocaleString('ru', {month: 'long'}).slice(0,-1) + 'я',
+      currentDayWeek = today.toLocaleString('ru', {weekday: 'long'}),
+      currentDay =  today.getDate(),
+      currentHour = today.getHours(),
+      currentMinute = today.getMinutes(),
+      currentSecond = today.getSeconds()
+;
+
+const signatureHour = function(currentHour) {
+    if (currentHour === 1 || currentHour === 21 || currentHour === 0) { return 'час';}
+    if (currentHour >= 2 && currentHour <= 4 || currentHour >=22 && currentHour <= 23) { return 'часa';}
+    if (currentHour >= 5 && currentHour <= 20) { return 'часов';}
+};
+
+
+let message = ('Сегодня ' + currentDayWeek + ", " + currentDay +
+ " " + currentMonth + " " +  currentYear +' года, ' + currentHour +
+  ' ' + signatureHour(currentHour) + ' ' + currentMinute + ' минут ' + currentSecond + " секунды");
+document.write(message );
+
+
+const date = new Date(),
+newDate = {
+      currentDateYear: today.getFullYear(),
+      currentDateMonth: today.getMonth(),
+      currentDateDayWeek: today.getDay(),
+      currentDateDay: today.getDate(),
+      currentDateHour: today.getHours(),
+      currentDateMinute: today.getMinutes(),
+      currentDateSecond: today.getSeconds()
+};
+
+//'04.02.2020 - 21:05:33' 
+
+for (let key in newDate) {
+  let value;
+  if ( (newDate[key]) < 10) {
+     newDate[key] = '0' + newDate[key];
   }
 }
-console.log(week);
 
-// я нашла метод .bold() - но он опять-таки ввыводит
-// в консоль значение в тегаз <b></b>  а не жирным, 
-// поэтому вывела на страницу. не знаю, насколько правильно,
-// в зд вообще написано на экран, а в каком виде не поняла, как сделала так и поняла..
+document.write(newDate.currentDateDay + '.' +
+  newDate.currentDateMonth  + '.' + newDate.currentDateYear +
+   ' - ' + newDate.currentDateHour + ':' + newDate.currentDateMinute + ':' +  newDate.currentDateSecond);
+};
+
+
+
+
+let displayText = document.createElement("p");
+document.body.appendChild(displayText);
+
+displayText.textContent = setInterval(function() {
+  nowDate();
+}, 1000);
